0180be7f384743eb20c544534da1d8908e9bdc26
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-09-11 11:20:59 +0800

    Rename `MailAgentTests`
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

--- src/test/java/com/jiandong/legendaryai/agent/mail/MailAgentTests.java
+++ src/test/java/com/jiandong/legendaryai/agent/mail/MailAgentTests.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class MailAgentTest {
+class MailAgentTests {
 
 	@Mock
 	ChatClient.Builder chatClientBuilder;
